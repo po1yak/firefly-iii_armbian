6f8e09d3468a213c5203b30f1b8bb9daf4fddc8a
po1yak <s.v.polyakov@gmail.com>
2018-04-30 00:29:00 +0000

    Add init-setup.sh

--- init-setup.sh
+++ init-setup.sh
@@ -14,12 +14,12 @@ apt-get update
 
 apt-get install docker-ce
 
-docker run -d -e 'MYSQL_ROOT_PASSWORD=q1w2e3r4t5y6' -e 'MYSQL_DATABASE=firefly_iii' -e 'MYSQL_USER=firefly_iii' -e 'MYSQL_PASSWORD=firefly_iii' -p '3306:3306' --name mysql po1yak/mysql_arm
+docker run -d --restart always -e 'MYSQL_ROOT_PASSWORD=q1w2e3r4t5y6' -e 'MYSQL_DATABASE=firefly_iii' -e 'MYSQL_USER=firefly_iii' -e 'MYSQL_PASSWORD=firefly_iii' -p '3306:3306' --name mysql po1yak/mysql_arm
 
 docker volume create firefly_iii_export
 docker volume create firefly_iii_upload
 
-docker run -d -v firefly_iii_export:/var/www/firefly-iii/storage/export -v firefly_iii_upload:/var/www/firefly-iii/storage/upload -p 80:80 -e FF_APP_ENV=local -e FF_APP_KEY=S0m3R@nd0mString0f32Ch@rsEx@ct1y -e FF_DB_HOST=192.168.0.88 -e FF_DB_NAME=firefly_iii -e FF_DB_USER=firefly_iii -e FF_DB_PASSWORD=firefly_iii --name firefly-iii po1yak/firefly-iii_arm
+docker run -d --restart always -v firefly_iii_export:/var/www/firefly-iii/storage/export -v firefly_iii_upload:/var/www/firefly-iii/storage/upload -p 80:80 -e FF_APP_ENV=local -e FF_APP_KEY=S0m3R@nd0mString0f32Ch@rsEx@ct1y -e FF_DB_HOST=192.168.0.88 -e FF_DB_NAME=firefly_iii -e FF_DB_USER=firefly_iii -e FF_DB_PASSWORD=firefly_iii --name firefly-iii po1yak/firefly-iii_arm
 
 docker exec -it firefly-iii php artisan migrate --seed
 docker exec -it firefly-iii php artisan firefly:upgrade-database
